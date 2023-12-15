4ae1e3f1850564c1a4656265828ea6c3ed9ad04e
2damb <franinfoprov@gmail.com>
2023-12-15 16:59:09 +0000

    componente cancion, iconos

--- app/src/main/java/com/franb/notspotify/vistas/ComponenteCancion.kt
+++ app/src/main/java/com/franb/notspotify/vistas/ComponenteCancion.kt
@@ -2,6 +2,13 @@ package com.franb.notspotify.vistas
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.KeyboardArrowLeft
+import androidx.compose.material.icons.rounded.PlayArrow
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
@@ -20,8 +27,29 @@ fun ComponenteCancion(){
         Text(text = "Now playing")
         Text(text = cancion.nombre)
         Image(painter = painterResource(id = cancion.imagen), contentDescription = null)
-        // TODO player
+        Slider(value = 0f, onValueChange = {}) // todo
+        Text(text = "0:00 / 2:53") // todo
 
+        Row {
+            // anterior, siguiente, play/stop, loop, shuffle
+            //loop
+            Button(onClick = { /*TODO*/ }) {
+                // TODO if looping icono relleno else icono normal
+            }
+
+            //anterior
+            Button(onClick = { /*TODO*/ }) {
+                Icon(Icons.Rounded.KeyboardArrowLeft, "Cancion anterior")
+            }
+
+            //play pause
+            Button(onClick = { /*TODO*/ }) {
+                // if not play...
+                Icon(Icons.Rounded.PlayArrow, "Play")
+                // else...
+                //Icon iconopause
+            }
+        }
 
     }
 }
