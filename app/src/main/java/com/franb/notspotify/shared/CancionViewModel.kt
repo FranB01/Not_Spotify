e905a3c6315da778900c07000ef8038a0a8abdc9
2damb <2damb@informatica.edu>
2023-12-15 16:26:17 +0000

    cosas

---
+++ app/src/main/java/com/franb/notspotify/shared/CancionViewModel.kt
@@ -0,0 +1,21 @@
+package com.franb.notspotify.shared
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class CancionViewModel: ViewModel(){
+    // Esta variable privada no es accesible desde fuera, y representa
+    // el flow de nuestra variable de estado.
+    private val _redondeo = MutableStateFlow(false)
+
+    // Esta variable publica recoge y actualiza el valor del estado mutable, y es el que usará
+    // la interfaz.
+    val redondeo = _redondeo.asStateFlow()
+
+    fun actualizarSwitch(nuevoValor : Boolean){
+        _redondeo.value = nuevoValor
+    }
+
+
+}
--- app/src/main/java/com/franb/notspotify/vistas/ComponenteCancion.kt
+++ app/src/main/java/com/franb/notspotify/vistas/ComponenteCancion.kt
@@ -1,8 +1,27 @@
 package com.franb.notspotify.vistas
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import com.franb.notspotify.Cancion
+import com.franb.notspotify.shared.CancionViewModel
 
 @Composable
 fun ComponenteCancion(){
+    val contexto = LocalContext.current
 
+    val viewModel: CancionViewModel = viewModel()
+    var cancion = Cancion("Ejemplo", 0,0)
+
+    Column {
+        Text(text = "Now playing")
+        Text(text = cancion.nombre)
+        Image(painter = painterResource(id = cancion.imagen), contentDescription = null)
+        // TODO player
+
+
+    }
 }
